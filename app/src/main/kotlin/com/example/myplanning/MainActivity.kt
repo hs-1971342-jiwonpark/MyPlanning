3d448e1140a64f5a8d36a71c77ea7b0a5a26cc74
jiwon <pjw99032986@gmail.com>
2025-01-02 19:58:27 +0900

    Feat : App Start
    시작 테마 적용 및 NavHost 적용

--- app/src/main/kotlin/com/example/myplanning/MainActivity.kt
+++ app/src/main/kotlin/com/example/myplanning/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.designsystem.theme.MyPlanningTheme
 import com.example.login.LoginRoute
 import dagger.hilt.android.AndroidEntryPoint
 import navigation.AppNavHost
+import ui.MainViewModel
 import ui.rememberMainAppState
 
 @AndroidEntryPoint
@@ -17,6 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val appState = rememberMainAppState()
+            val mainViewModel : MainViewModel = hiltViewModel()
             MyPlanningTheme {
                 AppNavHost(
                     appState = appState,
