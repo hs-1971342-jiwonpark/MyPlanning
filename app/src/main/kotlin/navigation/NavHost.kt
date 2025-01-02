34cd8fdcf141dc38794d684e21b52fc44a210455
jiwon <pjw99032986@gmail.com>
2025-01-02 20:00:50 +0900

    Feat : Main + NavHost
    앱 네비게이션 등록 NavHost 적용

--- app/src/main/kotlin/navigation/NavHost.kt
+++ app/src/main/kotlin/navigation/NavHost.kt
@@ -6,8 +6,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.login.loginScreen
+import com.example.myaccount.myAccountScreen
 import com.example.mypage.myPageScreen
 import com.example.planet.navigation.planetScreen
+import com.example.rule.ruleScreen
 import ui.MainApp
 import ui.MainAppState
 import ui.mainScreen
@@ -27,25 +29,17 @@ fun AppNavHost(
     ) {
         // 로그인 화면
         loginScreen(
-            context = context,
             navController = appState.navController
         )
-
         // 메인 화면
         mainScreen()
-
         // 로그인 -> 메인
         composable("main") { MainApp() }
-
         // Planet 화면
         planetScreen()
-
         // MyPage 화면
-        myPageScreen()
+        myPageScreen(navController = appState.navController)
+        myAccountScreen(navController = appState.navController)
+        ruleScreen(navController = appState.navController)
     }
 }
-
-
-
-
-
