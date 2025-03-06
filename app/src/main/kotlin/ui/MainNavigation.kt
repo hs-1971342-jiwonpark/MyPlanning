78b62cbf8ce6cbf55be58d54c654b6f4cfbb3a12
jiwon <pjw99032986@gmail.com>
2025-03-06 14:12:16 +0900

    refactor : 코드 정리
    코드 공백 정리

--- app/src/main/kotlin/ui/MainNavigation.kt
+++ app/src/main/kotlin/ui/MainNavigation.kt
@@ -1,22 +1,16 @@
 package ui
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.navDeepLink
-import com.example.login.LoginScreen
-import com.example.login.uri
 import com.example.navigation.Dest
 import com.example.navigation.FeatureGraph
 import com.example.navigation.NavigationDest
-import kotlinx.serialization.Serializable
 
 private const val MAIN_ROUTE = "main"
 private const val DEEP_LINK_URI = "myapp://main"
-
 
 interface MainFeature : FeatureGraph
 
@@ -24,7 +18,7 @@ class MainFeatureImpl : MainFeature {
     override fun navGraph(
         navHostController: NavHostController,
         navGraphBuilder: NavGraphBuilder,
-        provide : Any?
+        provide: Any?
     ) {
         navGraphBuilder.navigation<NavigationDest.MainRoute>(startDestination = Dest.MainRoute) {
             composable<Dest.MainRoute>(
