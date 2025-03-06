267277c2d1a5ffd53954600435d5916a1dd5a835
jiwon <pjw99032986@gmail.com>
2025-03-06 14:17:00 +0900

    feat : 코드 검토
    불필요 공백 제거 및 줄맞춤

--- common/navigation/src/main/java/com/example/navigation/FeatureGraph.kt
+++ common/navigation/src/main/java/com/example/navigation/FeatureGraph.kt
@@ -1,11 +1,12 @@
 package com.example.navigation
 
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 interface FeatureGraph {
-    fun navGraph(navHostController : NavHostController, navGraphBuilder: NavGraphBuilder, provide : Any?)
+    fun navGraph(
+        navHostController: NavHostController,
+        navGraphBuilder: NavGraphBuilder,
+        provide: Any?
+    )
 }
